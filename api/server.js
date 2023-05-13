f28b08d924713decc6f89cb57d49ac2677e570e4
Daniel <danielgd.bh@gmail.com>
2023-05-13 00:15:55 -0300

    att

--- api/server.js
+++ api/server.js
@@ -1,7 +1,8 @@
 // See https://github.com/typicode/json-server#module
 const jsonServer = require('json-server')
 const server = jsonServer.create()
-const router = jsonServer.router('../db.json')
+const path = require('path')
+const router = jsonServer.router(path.join(__dirname, 'db.json'))
 const middlewares = jsonServer.defaults()
 
 server.use(middlewares)
